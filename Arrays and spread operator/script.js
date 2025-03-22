a1acec22567a4a982a0f77d3230b071dd35b9932
Tausif Azhar Shaikh <tausif>
2025-03-22 12:11:58 +0530

    array and spread and rest operator

---
+++ Arrays and spread operator/script.js
@@ -0,0 +1,15 @@
+// Arrays is variable like conatiner which holds more than a single value
+
+// let fruits = ['apple','mango','banana','pineaple']
+
+// console.log(fruits.sort());
+
+// console.log(fruits.sort().reverse());
+
+
+// spread method
+
+let userName = "tausif azhar shaikh"
+
+let name = [...userName]
+console.log(name);
